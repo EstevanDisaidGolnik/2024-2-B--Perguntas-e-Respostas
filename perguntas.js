0966a21ad5182fdcdf0ae807879e80bed9db8736
Estevan Neri Disaid Emerenciano <141413662+EstevanDisaidGolnik@users.noreply.github.com>
2024-11-07 08:36:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    "Física",
+    "Qual a cíencia que estuda grandeza físicas?",
+    "A Física"
+)
+criaCartao(
+    "Química",
+    "Quais são os elementos necessários para um hidrocarboneto?",
+    "Hidrogênio e Carbono"
+)
+criaCartao(
+    "Games",
+    "Qual o melhor jogo do universo?",
+    "Minecraft"
+)
+criaCartao(
+    "História",
+    "Quem foi o maior ditador da Alemanha?",
+    "Adolf Hitler"
+)
+criaCartao(
+    "ED.Financeira",
+    "Qual a cryptomoeda mais valiosa?",
+    "O Bitcoin"
+)
+criaCartao(
+    "Biologia",
+    "Qual o maio osso do corpo humano?",
+    "O Fêmur"
+)
